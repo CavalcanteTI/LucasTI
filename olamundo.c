d33dceaa08aa9d3b0be1cb895c88b3d15a7fbe53
CavalcanteTI <lucasprogramacao.github@gmail.com>
2025-10-22 03:03:28 +0000

    Atualizando o cadastro das duas cartas

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main(){
+
+    int idade;
+    float altura;
+    double contaBancaria;
+    char letra;
+    char nome [20];
+
+    printf("Digite sua idade:");
+    scanf("%d", &idade);
+    printf("Sua idade é: %d\n", idade);
+    
+    
+    printf("Digite sua altura:");
+    scanf("%f", &altura);
+    printf("Sua altura é: %f\n", altura);
+
+
+    printf("Sua conta Bancária é : %.2f\n", contaBancaria);
+    printf("Letra do seu nome é : %c\n", letra);
+    printf("Seu nome é : %s\n", nome);
+ }
